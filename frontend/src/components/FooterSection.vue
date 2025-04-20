cc864e383a87dea448bcc7ee751f3da682693120
Mats Bakketeig <mats.bakketeig@outlook.com>
2025-04-20 22:30:37 +0200

    Add separate components for semantic elements

---
+++ frontend/src/components/FooterSection.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Info } from 'lucide-vue-next'
+</script>
+
+<template>
+  <footer class="p-1 h-1/40 flex items-center justify-between">
+    <p class="text-sm italic">Version 0.1.0-SNAPSHOT</p>
+    <Info
+      :size="18"
+    />
+  </footer>
+</template>
---
+++ frontend/src/components/HeaderSection.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { SquareCheck } from 'lucide-vue-next'
+</script>
+
+<template>
+  <header class="flex justify-center items-center h-1/8">
+    <SquareCheck
+      class="m-1 relative top-1"
+      :size="48"
+    />
+    <h1 class="text-6xl">Todo</h1>
+  </header>
+</template>
